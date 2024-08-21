470d3d99cbfc1a6710cb743f48ae0a8a83562815
Pratham Shailesh Dsouza <141055441+PrathamShailesh@users.noreply.github.com>
2024-08-21 11:05:09 +0530

    Update PasswordAuth.js

--- Server/Config/PasswordAuth.js
+++ Server/Config/PasswordAuth.js
@@ -48,7 +48,7 @@ passport.use(new GoogleStrategy({
         request.res.cookie("token", token, {
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true,  
-            secure: true,  
+            secure: false,  
             path: '/',
             sameSite: 'None'
         });
